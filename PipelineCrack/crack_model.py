2e97f7c370ee0a06704a7e2d0fed2c993d0a58c1
smadhiva <smadhivanan1234@gmail.com>
2025-03-09 10:50:42 +0530

    New Model

--- PipelineCrack/crack_model.py
+++ PipelineCrack/crack_model.py
@@ -2,15 +2,15 @@ import numpy as np
 
 def run_model(model, scaler, signal):
     # Convert to NumPy array
-    print(f"testing features print: {features}")
-    new_data = np.array(list(features.values()))
+    print(f"testing features print: {signal}")
+    new_data = np.array(list(signal.values()))
     
     # Print to verify
     #print(new_data)
     #print(type(new_data))
 
-    # Scale the new data
-    new_data_scaled = scaler.transform(new_data.reshape(-1, 1))
+    # # Scale the new data
+    # new_data_scaled = scaler.transform(new_data.reshape(-1, 1))
 
     # # Predict
     # prediction = xgb_model.predict(new_data_scaled)
