cd9138fbab445c5e6a62bccb40d200c4258aa0dd
smadhiva <smadhivanan1234@gmail.com>
2025-03-09 11:08:42 +0530

    New Logic

--- PipelineCrack/crack_model.py
+++ PipelineCrack/crack_model.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+from collections import Counter
+
+def check_correctness(signal):
+    # Round off values in the list
+    rounded_signal = [round(x) for x in signal]
+    
+    # Count occurrences of each unique element
+    count_dict = Counter(rounded_signal)
+    
+    return len(count_dict)
+
 def run_model(model, scaler, signal):
     # Convert to NumPy array
     print(f"testing features print: {signal}")
@@ -8,11 +19,15 @@ def run_model(model, scaler, signal):
     # Print to verify
     #print(new_data)
     #print(type(new_data))
-
+    print(f"Correlate : {scaler}")
     print(new_data.shape)
     # Scale the new data
     #new_data_scaled = scaler.transform(new_data.reshape(1,14))
     #print(new_data.reshape(1,14))
-    prediction = model.predict(new_data.reshape(1,14))
-
-    return prediction[0]
+    prediction = model.predict(new_data.reshape(1,14)) 
+    predict = True
+    if(prediction == 0.0):
+        predict = False
+    if scaler== 2:
+        return False
+    return True
--- PipelineCrack/engine.py
+++ PipelineCrack/engine.py
@@ -21,19 +21,21 @@ asyncio.run(main())  # This ensures the async function runs properly
 import asyncio
 from signal1 import *
 import joblib
-from crack_model import run_model
+from crack_model import run_model,check_correctness;
 from sklearn.preprocessing import StandardScaler
 xgb_model = joblib.load("xgboost_model.pkl")
 scaler = joblib.load("scaler.pkl")
 loaded_rf_model = joblib.load("random_forest_model.pkl")
+
 def get_signal():
     values, time_list = collect_signal()  # Blocking call (runs for 10s)
     return values
 
 async def get_crack_result():
     signal = await asyncio.to_thread(get_signal)  # Run get_signal() in a separate thread
+    signal_correalate = check_correctness(signal)
     data = get_features(signal)
-    result = run_model(loaded_rf_model, scaler, data)
+    result = run_model(loaded_rf_model,signal_correalate, data)
     return result
 
 async def main():
